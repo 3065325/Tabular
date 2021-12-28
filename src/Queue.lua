eb5c62e9707e8be820d1647de78043e09096ed6a
NPA <32378792+3065325@users.noreply.github.com>
2021-12-28 00:21:16 -1000

    Update Queue.lua

--- src/Queue.lua
+++ src/Queue.lua
@@ -1,9 +1,22 @@
 local Queue = {}
 Queue.__index = Queue
 
+function Queue.new()
+  local self = setmetatable({}, Queue)
+  
+  --Pointers
+  self.tail = 0
+  self.head = 1
+  
+  --Data
+  self.size = 0
+
+  return self
+end
+
 function Queue:PushBack(value)
     -- Add to tail
-    self.values[self.tail] = value
+    self[self.tail] = value
 
     -- Increment tail
     self.tail -= 1
@@ -14,7 +27,7 @@ end
 
 function Queue:PushFront(value)
     -- Add to head
-    self.values[self.head] = value
+    self[self.head] = value
 
     -- Increment head
     self.head += 1
@@ -46,15 +59,80 @@ function Queue:PopFront()
     -- Decrement size
     self.size -= 1
 
-    return self.values[self.head]
+    return self[self.head]
 end
 
 function Queue:Front()
-    return self.values[self.head - 1]
+    return self[self.head - 1]
 end
 
 function Queue:Back()
-    return self.values[self.tail + 1]
+    return self[self.tail + 1]
+end
+
+function Queue:Size()
+    return self.size
+end
+
+return Queue
+->
+local Queue = {}
+Queue.__index = Queue
+
+function Queue:PushBack(value)
+    -- Add to tail
+    self[self.tail] = value
+
+    -- Decrement tail
+    self.tail -= 1
+
+    -- Increment size
+    self.size += 1
+end
+
+function Queue:PushFront(value)
+    -- Add to head
+    self[self.head] = value
+
+    -- Increment head
+    self.head += 1
+
+    -- Increment size
+    self.size += 1
+end
+
+function Queue:PopBack()
+    -- Guard
+    if self.size == 0 then return end
+
+    -- Increment tail
+    self.tail += 1
+
+    -- Decrement size
+    self.size -= 1
+
+    return self[self.tail]
+end
+
+function Queue:PopFront()
+    -- Guard
+    if self.size == 0 then return end
+
+    -- Decrement head
+    self.head -= 1
+
+    -- Decrement size
+    self.size -= 1
+
+    return self[self.head]
+end
+
+function Queue:Front()
+    return self[self.head - 1]
+end
+
+function Queue:Back()
+    return self[self.tail + 1]
 end
 
 function Queue:Size()
